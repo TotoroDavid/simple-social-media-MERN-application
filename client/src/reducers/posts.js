d014c9b3f1d5892932ce713fb40dad1fc80cd8ee
TotoroDavid <davidtoro65@hotmail.com>
2021-09-07 15:07:50 -0500

    creating a testing the post part

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -8,7 +8,7 @@ export default (posts = [], action) => {
         case 'FETCH_ALL':
             return posts
         case 'CREATE':
-            return posts
+            return [...posts, action.payload]
         default:
             return posts
     }
