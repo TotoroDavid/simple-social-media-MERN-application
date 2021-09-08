7903439710a5f6e4f7f3fdda19d6f628760fd2fc
TotoroDavid <davidtoro65@hotmail.com>
2021-09-08 09:10:30 -0500

    deleting post

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,6 +4,7 @@
 
 import * as api from '../api'
 
+/**getAll the posts */
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts()
@@ -14,6 +15,7 @@ export const getPosts = () => async (dispatch) => {
     }
 }
 
+/**Create a new post */
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post)
@@ -24,11 +26,23 @@ export const createPost = (post) => async (dispatch) => {
     }
 }
 
+/**update post */
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post)
 
         dispatch({ type: 'UPDATE', payload: data })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+/**delete a post */
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+
+        dispatch({ type: 'DELETE', payload: id })
     } catch (error) {
         console.log(error)
     }
